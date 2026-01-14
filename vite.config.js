d16aa186fbed878e64f56cf4d7b54895705281aa
Adebambo Olowolekomo <105713906+Akayleo@users.noreply.github.com>
2026-01-14 09:37:05 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 
 export default defineConfig({
   base: '/fonction/', // This must match your GitHub repo name
+  plugins: [react()], // Added React plugin
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
